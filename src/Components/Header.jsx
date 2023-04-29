b990852c46b5e471b27b7f149ad6a6fd9821e9e2
Hexlat <albornozlucasariel13@gmail.com>
2023-04-29 00:59:46 -0300

    cambios en archivos y variables predeterminadas agregadas

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../Styles/header.css'
+
+const Header = () => {
+  return (
+    <>
+      <div>
+        <h1>H1 Headline</h1>
+        <h2>Esta configuracion esta en header.css</h2>
+        <h3>Las fuentes y colores predeterminados ya los establecí.</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quia consectetur modi error, soluta odio. Reiciendis itaque illum fugit ratione. At quod quos consequuntur impedit? Asperiores cum necessitatibus vel recusandae.</p>
+      </div>
+    </>
+  )
+}
+
+export default Header
